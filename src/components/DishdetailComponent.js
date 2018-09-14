d036c27a886bdb6dafadce61741f2517022abaa5
Mohammad Moghimi <moghimimohammad@gmail.com>
2018-09-14 12:34:42 +0100

    Assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -34,17 +35,27 @@ const  DishDetail = (props) => {
 	const { dish } = props;
 	if(dish != null) {
 		return (
-			<div className="container">
-				<div className="row">
-	              <div  className="col-12 col-md-5 m-1">
-	                <RenderDish dish={dish} />
-	              </div>
-	              <div  className="col-12 col-md-5 m-1">
-	              	<h4>Comments</h4>
-	                <RenderComments comments={dish.comments} />
-	              </div>
-	            </div>
-			</div>		            
+            <div className="container">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+            </div>
         );
 	} else {
 		return (<div></div>);
